b492873a047f60bd7d0e66b547821e86debd361b
jcarlosruizgar <jcarlosruizgar@gmail.com>
2022-01-04 20:03:39 +0100

    update

--- Tres en raya/src/codigo/Main.java
+++ Tres en raya/src/codigo/Main.java
@@ -1,19 +1,54 @@
+/*
+@author Juan Carlos Ruiz García
+@date 04/01/2022
+ */
 package codigo;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) {
-        boolean juegoFinalizado = false;
-
-        TresEnRaya juego = new TresEnRaya();
-
-        do {
-            juego.pintar();
-            juego.jugar();
-            juegofinalizado=juego.Comprobar3EnRaya() || juego.tablerotaLleno();
-
-        } while (!juegoFinalizado);
-
+        //variables
+        boolean juegoFinalizado = false;//determina cuando se acaba la partida
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int posicionJugar = 0;//determina la posicion en la que se va a jugar
+        TresEnRaya juego = new TresEnRaya();//llamada al constructor con el tablero vacio
+        //ejecucion del programa
+        System.out.println("3 en Raya");
+        do{//bucle con la ejecucion de los turnos
+            juego.pintarTablero();
+            if(!juego.isTurno()){//turno jugador
+                System.out.println("Introduzca del 1 al 9 para elegir posición:");
+                try{
+                    do{//bucle para elegir una casilla valida
+                        posicionJugar = Integer.parseInt(br.readLine());
+                        if(posicionJugar <1 || posicionJugar > 9) System.out.println("Esa no es una posición valida.\nIntroduzca del 1 al 9 para elegir posición otra vez:");//si la casilla no es valida
+                        if(juego.casillaOcupada(posicionJugar)) System.out.println("Esa casilla ya esta ocupada.\nIntroduzca del 1 al 9 para elegir posición otra vez:");//si la casilla esta ocupada
+                    }
+                    while(juego.casillaOcupada(posicionJugar) || (posicionJugar < 1 || posicionJugar > 9));
+                }
+                catch (IOException ioe){
+                    System.out.println("Ese no es un valor valido.");
+                }
+            }
+            else{//turno maquina
+                System.out.println("Turno de la máquina:");
+                do{//bucle para que la maquina juegue una casilla valida
+                    posicionJugar = (int)Math.floor(Math.random()*9+1);//casilla aleatoria entre 1 y 9
+                }
+                while(juego.casillaOcupada(posicionJugar) || (posicionJugar < 1 || posicionJugar > 9));
+            }
+            juego.jugada(posicionJugar);//asigna a la casilla X o O dependiendo del turno
+            juegoFinalizado = juego.tableroLleno() || juego.comprobar3EnRaya();//comprueba si se cumple condicion para finalizar el juego
+            juego.setTurno(!juego.isTurno());//cambia el turno para la siguiente jugada
+        }
+        while(!juegoFinalizado);
+        juego.pintarTablero();//muestra la ultima jugada cuando se acaba el juego
+        if(juego.comprobar3EnRaya() && juego.isTurno()) System.out.println("¡Has ganado!");//mensaje si gana el jugador
+        if(juego.comprobar3EnRaya() && !juego.isTurno()) System.out.println("¡Has perdido!");//mensaje si gana la maquina
+        if(!juego.comprobar3EnRaya() && juego.tableroLleno()) System.out.println("¡Empate!");//mensaje si se queda empate
     }
-
 }
--- Tres en raya/src/codigo/TresEnRaya.java
+++ Tres en raya/src/codigo/TresEnRaya.java
@@ -1,12 +1,12 @@
 package codigo;
 
-
 public class TresEnRaya {
     private char tablero[][];
     private boolean turno;
 
+    //constructor
     public TresEnRaya() {
-        turno = true;
+        turno = false;
         tablero = new char [3][3];
 
         for (int i = 0; i < 3; i++) {
@@ -15,39 +15,138 @@ public class TresEnRaya {
             }
         }
     }
-
-    public void pintar() {
-
+    //metodo para mostrar el tablero en su estado actual
+    public void pintarTablero() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print("|" + this.tablero[i][j]);
+                if(j == 2) System.out.print("|");
+            }
+            if(i != 2) System.out.println("\n-------");
+            else System.out.println("");
+        }
     }
 
+    //metodo que comprueba si el tablero esta lleno
     public boolean tableroLleno() {
-        //return
-
+        boolean lleno = true;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if(this.tablero[i][j] == ' ') lleno = false;
+            }
+        }
+        return lleno;
     }
 
+    //metodo que comprueba si hay linea de algun jugador
     public boolean comprobar3EnRaya() {
-        //return
+        boolean linea = false;
+        //lineas horizontales X J1
+        if (this.tablero[0][0] == 'X' && this.tablero[0][1] == 'X' && this.tablero[0][2] == 'X') return true;
+        if (this.tablero[1][0] == 'X' && this.tablero[1][1] == 'X' && this.tablero[1][2] == 'X') return true;
+        if (this.tablero[2][0] == 'X' && this.tablero[2][1] == 'X' && this.tablero[2][2] == 'X') return true;
+        //lineas horizontales O J2
+        if (this.tablero[0][0] == 'O' && this.tablero[0][1] == 'O' && this.tablero[0][2] == 'O') return true;
+        if (this.tablero[1][0] == 'O' && this.tablero[1][1] == 'O' && this.tablero[1][2] == 'O') return true;
+        if (this.tablero[2][0] == 'O' && this.tablero[2][1] == 'O' && this.tablero[2][2] == 'O') return true;
+        //lineas verticales X J1
+        if (this.tablero[0][0] == 'X' && this.tablero[1][0] == 'X' && this.tablero[2][0] == 'X') return true;
+        if (this.tablero[0][1] == 'X' && this.tablero[1][1] == 'X' && this.tablero[2][1] == 'X') return true;
+        if (this.tablero[0][2] == 'X' && this.tablero[1][2] == 'X' && this.tablero[2][2] == 'X') return true;
+        //lineas verticales O J2
+        if (this.tablero[0][0] == 'O' && this.tablero[1][0] == 'O' && this.tablero[2][0] == 'O') return true;
+        if (this.tablero[0][1] == 'O' && this.tablero[1][1] == 'O' && this.tablero[2][1] == 'O') return true;
+        if (this.tablero[0][2] == 'O' && this.tablero[1][2] == 'O' && this.tablero[2][2] == 'O') return true;
+        //lineas diagonales X J1
+        if(this.tablero[0][0] == 'X' && this.tablero[1][1] == 'X' && this.tablero[2][2] == 'X') return true;
+        if(this.tablero[2][0] == 'X' && this.tablero[1][1] == 'X' && this.tablero[0][2] == 'X') return true;
+        //lineas diagonales O J2
+        if(this.tablero[0][0] == 'O' && this.tablero[1][1] == 'O' && this.tablero[2][2] == 'O') return true;
+        if(this.tablero[2][0] == 'O' && this.tablero[1][1] == 'O' && this.tablero[0][2] == 'O') return true;
+        return linea;
     }
 
-    public char[][] getTablero() {
-        return tablero;
+    //metodo para jugar, convierte numero de 1 a 9 en las posiciones del array
+    public void jugada(int numero){
+        int a = 0;
+        int b = 0;
+        if(numero == 1){
+            a = 0; b = 0;
+        }
+        else if(numero == 2){
+            a = 0; b = 1;
+        }
+        else if(numero == 3){
+            a = 0; b = 2;
+        }
+        else if(numero == 4){
+            a = 1; b = 0;
+        }
+        else if(numero == 5){
+            a = 1; b = 1;
+        }
+        else if(numero == 6){
+            a = 1; b = 2;
+        }
+        else if(numero == 7){
+            a = 2; b = 0;
+        }
+        else if(numero == 8){
+            a = 2; b = 1;
+        }
+        else if(numero == 9){
+            a = 2; b = 2;
+        }
+        if(this.turno == false) this.tablero[a][b] = 'X';
+        else this.tablero[a][b] = 'O';
     }
 
-    public void setTablero(char[][] tablero) {
-        this.tablero = tablero;
-    }
-
+    //devuelve el turno
     public boolean isTurno() {
         return turno;
     }
 
+    //asigna el turno
     public void setTurno(boolean turno) {
         this.turno = turno;
     }
 
-    public void jugar() {
-        // TODO Auto-generated method stub
-
+    //metodo que comprueba si la casilla elegida esta ocupada
+    public boolean casillaOcupada(int numero){
+        boolean ocupada = false;
+        int a = 0;
+        int b = 0;
+        if(numero == 1){
+            a = 0; b = 0;
+        }
+        else if(numero == 2){
+            a = 0; b = 1;
+        }
+        else if(numero == 3){
+            a = 0; b = 2;
+        }
+        else if(numero == 4){
+            a = 1; b = 0;
+        }
+        else if(numero == 5){
+            a = 1; b = 1;
+        }
+        else if(numero == 6){
+            a = 1; b = 2;
+        }
+        else if(numero == 7){
+            a = 2; b = 0;
+        }
+        else if(numero == 8){
+            a = 2; b = 1;
+        }
+        else if(numero == 9){
+            a = 2; b = 2;
+        }
+        if(this.tablero[a][b] != ' '){
+            ocupada = true;
+        }
+        return ocupada;
     }
 
 }
