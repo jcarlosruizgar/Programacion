3a042dc069b01745dcda7f5538008e979996a427
jcarlosruizgar <jcarlosruizgar@gmail.com>
2022-01-19 13:28:10 +0100

    update

--- Tema 7/src/EjAgregacion/Principal.java
+++ Tema 7/src/EjAgregacion/Principal.java
@@ -2,14 +2,14 @@ package EjAgregacion;
 
 public class Principal {
 
-    private static final int NUMERO= 10;
+    private static final int NUMERO= 50;
 
     public static void main(String[] args) {
 
         Piso delPocero[] = new Piso[NUMERO];
 
         for (int i = 0; i < NUMERO; i++) {
-            delPocero[i] = new Piso((int)Math.floor(Math.random()*10));
+            delPocero[i] = new Piso((int)Math.floor(Math.random()*(200-40))+40);
         }
 
     }
