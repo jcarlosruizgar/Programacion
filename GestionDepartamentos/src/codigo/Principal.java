413c2b9d7524d9a40c9db914c8c9748e749c888b
jcarlosruizgar <jcarlosruizgar@gmail.com>
2022-02-16 23:11:08 +0100

    update

--- GestionDepartamentos/src/codigo/Principal.java
+++ GestionDepartamentos/src/codigo/Principal.java
@@ -21,4 +21,58 @@ public class Principal {
     private static int posSub;//posicion del array tomada en el submenu
     private static int posicionInserciones;//posicion donde se va a hacer la proxima insercion
 
+    public static void main(String[] args) {
+
+        System.out.println("Aplicacion de gestion de departamentos version 1.0");
+        try{
+            do{//bucle do while con la ejecucion del programa
+                calcularInsercion();
+                System.out.println("Seleccione la operacion a realizar:\nMenu:");
+                System.out.println("1 - Mostrar departamento.");
+                System.out.println("2 - Insertar un departamento.");
+                System.out.println("3 - Borrar un departamento.");
+                System.out.println("4 - Modificar atributo de un departamento.");
+                System.out.println("5 - Salir.");
+                opcionMenu = Integer.parseInt(br.readLine());
+                switch(opcionMenu){
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5://opcion salir
+                        salir=true;
+                        System.out.println("Saliendo del programa.");
+                        break;
+                }
+
+
+            }
+            while(!salir);//salida de la ejecucion del programa
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    //metodo para calcular la posicion a insertar
+    static void calcularInsercion(){
+        boolean noEncontrado = false;
+        int i = 0;
+        do{
+            if(listadoDepartamentos[i] == null){
+                posicionInserciones = i;
+                noEncontrado = true;
+            }
+            else i++;
+        }
+        while(!noEncontrado && i<TAMANO);
+        if(!noEncontrado) posicionInserciones = -1;
+    }
+
+
+
 }
