f88a46288d9ea53035a92e1c180be6a1517499c8
jcarlosruizgar <jcarlosruizgar@gmail.com>
2022-03-13 12:05:17 +0100

    update

--- Gestion de departamentos y empleados/src/codigo/Principal.java
+++ Gestion de departamentos y empleados/src/codigo/Principal.java
@@ -592,6 +592,7 @@ public class Principal implements operacionesDepartamento,operacionesEmpleado {
                         System.out.println("5 - Para comision del director.");
                     }
                     datos[2] = Integer.parseInt(br.readLine());
+                    p.modificarDepartamento(datos);
                 }
             }
         }
@@ -600,14 +601,19 @@ public class Principal implements operacionesDepartamento,operacionesEmpleado {
     public void modificarEmpleado(int[] datos) throws IOException {
         switch (datos[2]){
             case 1:
+                System.out.println("1");
                 break;
             case 2:
+                System.out.println("2");
                 break;
             case 3:
+                System.out.println("3");
                 break;
             case 4:
+                System.out.println("4");
                 break;
             case 5:
+                System.out.println("5");
                 break;
         }
     }
