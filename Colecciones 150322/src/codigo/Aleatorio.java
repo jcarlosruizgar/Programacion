035ab7fb4d6f583c7f656322749216a48a592642
jcarlosruizgar <jcarlosruizgar@gmail.com>
2022-03-16 23:12:55 +0100

    update

--- Colecciones 150322/src/codigo/Aleatorio.java
+++ Colecciones 150322/src/codigo/Aleatorio.java
@@ -5,8 +5,8 @@ import java.util.ArrayList;
 public class Aleatorio {
 
     public static void main(String[] args) {
-        //array list de 10 numeros enteros, que cargue los 10 numeros de forma aleatoria y los muestre
 
+        //array list de 10 numeros enteros, que cargue los 10 numeros de forma aleatoria y los muestre
         ArrayList<Double> aleatorios = new ArrayList<Double>();
 
         for (int i = 0; i < 10; i++) {
