268fe36b83051924530fa98418ce22e8ac1c1342
jcarlosruizgar <jcarlosruizgar@gmail.com>
2022-03-17 23:08:28 +0100

    update

--- Colecciones 150322/src/codigo/Aleatorio.java
+++ Colecciones 150322/src/codigo/Aleatorio.java
@@ -1,22 +1,64 @@
 package codigo;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Aleatorio {
+
+    private static ArrayList<Double> aleatorios = new ArrayList<Double>();
 
     public static void main(String[] args) {
 
         //array list de 10 numeros enteros, que cargue los 10 numeros de forma aleatoria y los muestre
-        ArrayList<Double> aleatorios = new ArrayList<Double>();
+
 
         for (int i = 0; i < 10; i++) {
             aleatorios.add(Math.random()*100);
         }
 
-        for(Double aleatorio:aleatorios){
+        for(double aleatorio:aleatorios){
             System.out.println(aleatorio);
         }
 
+        System.out.println(numeroMenor());
+        System.out.println(posicionNumeroMenor(numeroMenor()));
+
+        System.out.println(aleatorios.size());
+
+        Collections.sort(aleatorios);
+        for(double aleatorio:aleatorios){
+            System.out.println(aleatorio);
+        }
+
+        System.out.println(media());
+
+
+        //debe decir, el menor, el mayor, la media y la cantidad de numeros que tiene
+
+
+
+    }
+
+    public static double numeroMenor(){
+        double numMenor = aleatorios.get(0);
+        for (int i = 1; i < aleatorios.size(); i++) {
+            if(numMenor > aleatorios.get(i)){
+                numMenor = aleatorios.get(i);
+            }
+        }
+        return numMenor;
+    }
+
+    public static int posicionNumeroMenor(double numeroBuscado){
+        return aleatorios.indexOf(numeroBuscado);
+    }
+
+    public static double media(){
+        double suma = 0;
+        for (double numero:aleatorios){
+            suma += numero;
+        }
+        return suma/aleatorios.size();
     }
 
 }
---
+++ Colecciones 150322/src/codigo/MenuArrayList.java
@@ -0,0 +1,87 @@
+package codigo;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/**
+ * @author Juan Carlos
+ * @date 17/03/2022
+ * @version 1
+ * @description menu interactivo, añadir, borrar, modificar numeros
+ */
+public class MenuArrayList {
+
+    private static boolean salir = false;
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static ArrayList<Double> numeros = new ArrayList<Double>();
+
+    public static void main(String[] args) {
+        System.out.println("Aplicacion operaciones Array List:");
+        do{
+            try{
+                System.out.println("Introduzca:");
+                System.out.println("1 - Para mostrar los numeros almacenados.");
+                System.out.println("2 - Para añadir un numero.");
+                System.out.println("3 - Para borrar un numero.");
+                System.out.println("4 - Para modificar un numero.");
+                System.out.println("0 - Para salir.");
+                int seleccionMenu = Integer.parseInt(br.readLine());
+                switch (seleccionMenu){
+                    case 1:
+                        mostrarNumeros();
+                        break;
+                    case 2:
+                        anadirNumero();
+                        break;
+                    case 3:
+                        borrarNumero();
+                        break;
+                    case 4:
+                        modificarNumero();
+                        break;
+                    case 0:
+                        salir = true;
+                        break;
+                }
+            }
+            catch (NumberFormatException nfe){
+                System.out.println("Eso no es un numero.");
+            }
+            catch (Exception e){
+                e.printStackTrace();
+            }
+        }while (!salir);
+    }
+
+    public static void mostrarNumeros(){
+        System.out.println("Los numeros almacenados son:");
+        for(Double numero:numeros){
+            System.out.println("\t" + numero);
+        }
+    }
+
+    public static void anadirNumero() throws IOException {
+        System.out.println("Introduzca el numero que quiere añadir:");
+        numeros.add(Double.parseDouble(br.readLine()));
+    }
+
+    public static void borrarNumero() throws IOException {
+        System.out.println("Introduzca el numero que quiere borrar:");
+        numeros.remove(Double.parseDouble(br.readLine()));
+    }
+
+    public static void modificarNumero() throws IOException {
+        System.out.println("introduzca el numero que quiere modificar:");
+        int numeroBuscado = numeros.lastIndexOf(Double.parseDouble(br.readLine()));
+        if (numeroBuscado == -1){
+            System.out.println("El numero buscado " + numeroBuscado + " no esta registrado.");
+        }
+        else{
+            System.out.println("Introduzca el nuevo numero:");
+            numeros.set(numeroBuscado,Double.parseDouble(br.readLine()));
+        }
+    }
+
+}
