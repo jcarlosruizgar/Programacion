07500c4a485a5f8cdb0b60d56fc663cfcc819d42
jcarlosruizgar <jcarlosruizgar@gmail.com>
2022-04-03 17:12:53 +0200

    update

--- Genericos/src/codigo/PruebaComentarios.java
+++ Genericos/src/codigo/PruebaComentarios.java
@@ -1,6 +1,7 @@
 package codigo;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 public class PruebaComentarios {
 
@@ -10,12 +11,12 @@ public class PruebaComentarios {
 
 
 
-        Comentario c1 = new Comentario("perico","perez", LocalDate.of(2020,10,05),"hola fgfhfg ghjghjd dfhgf ");
-        Comentario c2 = new Comentario("patricia","algarrobo", LocalDate.of(2020,10,05),"hola fhg jgf");
-        Comentario c3 = new Comentario("alberto","castellon", LocalDate.of(2020,10,05),"df dh fsgj on es5huh 9e5hg esh8g5e ege");
-        Comentario c4 = new Comentario("isabel","godoy", LocalDate.of(2020,10,05),"holdfgdsh asdrhfghgf");
+        Comentario c1 = new Comentario("perico","perez", LocalDate.of(2020,10,05),"Lorem Ipsum is simply dummy text");
+        Comentario c2 = new Comentario("patricia","algarrobo", LocalDate.of(2020,10,05),"t is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout");
+        Comentario c3 = new Comentario("alberto","castellon", LocalDate.of(2021,10,05),"There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour");
+        Comentario c4 = new Comentario("isabel","godoy", LocalDate.of(2020,10,05),"The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested");
 
-        System.out.println(pilaComentario.estaVacia());
+        //System.out.println(pilaComentario.estaVacia());
 
         pilaComentario.aniadir(c1);
         pilaComentario.aniadir(c2);
@@ -32,8 +33,9 @@ public class PruebaComentarios {
         System.out.println(pc);
          */
 
-        System.out.println("El comentario mas largo es: " + comentarioMasLargo());
-        System.out.println(autorComentarioMasLargo());
+        //System.out.println("El comentario mas largo es: " + comentarioMasLargo());
+        //System.out.println(autorComentarioMasLargo());
+        System.out.println(comentarioDelAnio("2020"));
 
     }
 
@@ -65,6 +67,16 @@ public class PruebaComentarios {
             while(contador < pilaComentario.getPila().size());
             return "El comentario mas largo ha sido hecho por: " +nombreApellido;
         }
+    }
+
+    public static ArrayList<String> comentarioDelAnio(String anio){
+        ArrayList<String> listaComentarios = new ArrayList<>();
+        for(Comentario comentario: pilaComentario.getPila()){
+            if (comentario.getFecha().getYear()==Integer.parseInt(anio)){
+                listaComentarios.add(comentario.toString()+"\n");
+            }
+        }
+        return listaComentarios;
     }
 
 }
