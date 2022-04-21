a6554cdc7b055f0ba4a4db07ae5997cbb983e9f1
jcarlosruizgar <jcarlosruizgar@gmail.com>
2022-04-21 18:36:15 +0200

    update

--- Alumnos/src/codigo/Principal.java
+++ Alumnos/src/codigo/Principal.java
@@ -1,10 +1,14 @@
 package codigo;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Iterator;
 import java.util.TreeSet;
 
 public class Principal {
 
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     private static TreeSet<Alumno> aula1 = new TreeSet<Alumno>();
     private static TreeSet<Alumno> aula2 = new TreeSet<Alumno>();
     private static TreeSet<Alumno> aula3 = new TreeSet<Alumno>();
@@ -12,58 +16,83 @@ public class Principal {
     private static Aula aulaDos = new Aula();
 
     public static void main(String[] args) {
-
+        /*
         Alumno a1 = new Alumno("pedro","perez",26,"badajoz");
         Alumno a2 = new Alumno("maria","garcia",36,"madrid");
         Alumno a3 = new Alumno("fernando","ruiz",22,"merida");
         Alumno a4 = new Alumno("luis","gallardo",19,"don benito");
-
         Alumno a5 = new Alumno("luis","xcvs",29,"zafra");
         Alumno a6 = new Alumno("jghf","dfghfgh",33,"caceres");
         Alumno a7 = new Alumno("dfgdf","fgfdhd",21,"plasencia");
         Alumno a8 = new Alumno("jhjgh","gfghgf",22,"barcelona");
-
         System.out.println(insertarAlumno(a1));
         System.out.println(insertarAlumno(a2));
         System.out.println(insertarAlumno(a3));
         System.out.println(insertarAlumno(a4));
-
         aula2.add(a5);
         aula2.add(a6);
         aula2.add(a7);
         aula2.add(a8);
-
-
-
-
-        insertarAlumnos(aula1,aula2);
-
+        */
+        //insertarAlumnos(aula1,aula2);
         //mostrarAlumnos();
         //mostrarAlumnosIt();
-
-        aulaDos.insertarAulumnos(aula1);
-
+        //aulaDos.insertarAulumnos(aula1);
         //System.out.println(aulaDos);
-
-        clonarAula(aula2,aula3);
+        //clonarAula(aula2,aula3);
         /*for(Alumno alumno: aula3){
             System.out.println(alumno);
         }*/
-
+        /*
         System.out.println(borrarAlumno(a1));
         System.out.println(borrarAlumno(a6));
-
         mostrarAlumnos();
-
         System.out.println(primerAlumno());
         System.out.println(ultimoAlumno());
-
-
+        */
+        try{
+            switch (menu()){
+                case 1:
+                    insertarAlumno();
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                default:
+                    System.out.println("Eso no es una opcion valida.");
+                    break;
+            }
+        }
+        catch (IOException ioe){
+            System.out.println("Error en la entrada de datos.");
+        }
     }
 
-    public static int insertarAlumno(Alumno a){
+    public static int insertarAlumnoOperativo(Alumno a){
         if (aula1.add(a)) return 1;
         else return 0;
+    }
+
+    public static void insertarAlumno() throws IOException{
+        String nombre;
+        String apellido;
+        int edad = 0;
+        String poblacion;
+        System.out.println("Introduzca el nombre del alumno:");
+        nombre = br.readLine();
+        System.out.println("Introduzca el apellido del alumno:");
+        apellido = br.readLine();
+        System.out.println("Introduzca la edad del alumno:");
+        try{
+            edad = Integer.parseInt(br.readLine());
+        }
+        catch (NumberFormatException nfe){
+            System.out.println("Eso no es un numero valido.");
+        }
+        System.out.println("Introduza la poblacion del alumno:");
+        poblacion = br.readLine();
+        insertarAlumnoOperativo(new Alumno(nombre,apellido,edad,poblacion));
     }
 
     public static void mostrarAlumnos(){
@@ -102,6 +131,21 @@ public class Principal {
 
     public static Alumno ultimoAlumno(){
         return aula1.last();
+    }
+
+    public static int menu(){
+        int opcion;
+        System.out.println("Seleccione una operacion a realizar:");
+        System.out.println("1- Insertar un alumno");
+        System.out.println("2- Borrar un alumno");
+        System.out.println("3- Mostrar los alumnos");
+        try{
+            return Integer.parseInt(br.readLine());
+        }
+        catch (IOException ioe){
+            System.out.println("Error en la entrada de datos.");
+            return 0;
+        }
     }
 
 }
--- Mapas/src/codigo/Principal.java
+++ Mapas/src/codigo/Principal.java
@@ -1,41 +1,75 @@
 package codigo;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.HashMap;
 
 public class Principal {
 
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static HashMap<Integer,Alumno> mapa = new HashMap<>();
+
     public static void main(String[] args) {
 
+        /*
         Alumno a1 = new Alumno("Perico",6);
         Alumno a2 = new Alumno("Fran",7);
         Alumno a3 = new Alumno("Paco",8.2);
         Alumno a4 = new Alumno("Perica",5);
         Alumno a5 = new Alumno("Francisca",3);
         Alumno a6 = new Alumno("Paca",7);
-
-        HashMap<Integer,Alumno> mapa = new HashMap<>();
-
         mapa.put(1,a1);
         mapa.put(2,a2);
         mapa.put(3,a3);
         mapa.put(4,a4);
         mapa.put(5,a5);
         mapa.put(6,a6);
-
         System.out.println(mapa.get(4));
-
         mapa.remove(4);
-
         System.out.println(mapa);
-
         System.out.println(mapa.get(3));
-
         mapa.remove(4);
-
         System.out.println(mapa);
-
         mapa.clear();
+         */
 
+    }
+
+    public static void menu(){
+        int opcion;
+        System.out.println("Seleccione una operacion a realizar:");
+        System.out.println("1- Insertar un alumno");
+        System.out.println("2- Borrar un alumno");
+        System.out.println("3- Mostrar los alumnos");
+        try{
+            opcion = Integer.parseInt(br.readLine());
+        }
+        catch (IOException ioe){
+            System.out.println("Error en la entrada de datos.");
+        }
+    }
+
+    public static void insertarAlumno(){
+        String nombre;
+        double nota;
+        int clave;
+        System.out.println("Introduzca el nombre del alumno:");
+        try{
+            nombre = br.readLine();
+        }
+        catch (IOException ioe){
+            System.out.println("Error en la entrada de datos.");
+        }
+        System.out.println("Introduzca la nota del alumno:");
+        try{
+            nota = Double.parseDouble(br.readLine());
+        }
+        catch ()
+    }
+
+    public static void instertarAlumnoOperativo(String nombre,double nota,int clave){
+        mapa.put(clave,new Alumno(nombre,nota));
     }
 
 }
