8d00b64aef09da14f4496700bdd777605b4378e7
jcarlosruizgar <jcarlosruizgar@gmail.com>
2022-04-25 09:12:33 +0200

    update

--- Fracciones/src/codigo/Principal.java
+++ Fracciones/src/codigo/Principal.java
@@ -6,8 +6,8 @@ public class Principal {
 
     public static void main(String[] args) {
 
-        Fraccion<Integer> f1 = new Fraccion<>(valueOf(6),valueOf(2));
-        Fraccion<Integer> f2 = new Fraccion<>(valueOf(3),valueOf(3));
+        Fraccion<Integer> f1 = new Fraccion<>(6,2);
+        Fraccion<Integer> f2 = new Fraccion<>(3,3);
 
         System.out.println(f1);
         System.out.println(f2);
