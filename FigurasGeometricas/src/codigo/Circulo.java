29f9bd740f05dc72c9d3905fb12841d99c1b1478
jcarlosruizgar <jcarlosruizgar@gmail.com>
2022-05-16 14:06:32 +0200

    update

---
+++ FigurasGeometricas/src/codigo/Circulo.java
@@ -0,0 +1,27 @@
+package codigo;
+
+public class Circulo extends Punto{
+
+    private int radio;
+
+    public Circulo() {
+    }
+
+    public Circulo(int x, int y, int radio) {
+        super(x, y);
+        this.radio = radio;
+    }
+
+    public int getRadio() {
+        return radio;
+    }
+
+    public void setRadio(int radio) {
+        this.radio = radio;
+    }
+
+    @Override
+    public String toString() {
+        return "Circulo con punto x="+super.getX()+" con punto y="+super.getY()+" y con un radio de "+this.radio;
+    }
+}
--- FigurasGeometricas/src/codigo/Principal.java
+++ FigurasGeometricas/src/codigo/Principal.java
@@ -10,8 +10,11 @@ public class Principal {
     private static FileInputStream fis = null;
     private static ObjectInputStream entrada = null;
     private static ArrayList<Rectangulo> listaRectangulos = new ArrayList<Rectangulo>();
-    private static final File RUTA = new File("./rectangulos.dat");
-    private static final File RUTA_INFORME = new File("./rectangulos.txt");
+    private static ArrayList<Circulo> listaCirculos = new ArrayList<Circulo>();
+    private static final File RUTA_RECTANGULOS = new File("./rectangulos.dat");
+    private static final File RUTA_CIRCULOS = new File("./circulos.dat");
+    private static final File RUTA_INFORME_RECTANGULOS = new File("./rectangulos.txt");
+    private static final File RUTA_INFORME_CIRCULOS = new File("./circulos.txt");
     private static FileWriter fw = null;
 
     public static void main(String[] args) {
@@ -20,7 +23,12 @@ public class Principal {
             escribirRectangulo();
             leerRectangulo();
             mostrarRectangulos();
-            crearInforme();
+            crearInformeRectangulos();
+            escribirCirculo();
+            leerCirculo();
+            mostrarCirculos();
+            crearInformeCirculos();
+            mostrarFicheroTexto();
         }
         finally {
             try{
@@ -47,7 +55,7 @@ public class Principal {
 
     public static void escribirRectangulo(){
         try{
-            fos = new FileOutputStream(RUTA);
+            fos = new FileOutputStream(RUTA_RECTANGULOS);
             salida = new ObjectOutputStream(fos);
             listaRectangulos.add(new Rectangulo(12,6,"azul"));
             listaRectangulos.add(new Rectangulo(new Punto(4,2),60,25,"morado"));
@@ -65,7 +73,7 @@ public class Principal {
 
     public static void leerRectangulo(){
         try{
-            fis = new FileInputStream(RUTA);
+            fis = new FileInputStream(RUTA_RECTANGULOS);
             entrada = new ObjectInputStream(fis);
             listaRectangulos = (ArrayList<Rectangulo>)entrada.readObject();
             fis.close();
@@ -87,10 +95,10 @@ public class Principal {
         }
     }
 
-    public static void crearInforme(){
+    public static void crearInformeRectangulos(){
         int contador = 1;
         try{
-            fw = new FileWriter(RUTA_INFORME);
+            fw = new FileWriter(RUTA_INFORME_RECTANGULOS);
             for(Rectangulo r:listaRectangulos){
                 fw.write("Rectangulo nº"+contador+":\n");
                 fw.write(r.toString()+"\n\n");
@@ -99,6 +107,90 @@ public class Principal {
             fw.close();
         }catch (IOException ioe){
             System.out.println("Error E/S.");
+        }
+    }
+
+    public static void escribirCirculo(){
+        try{
+            fos = new FileOutputStream(RUTA_CIRCULOS);
+            salida = new ObjectOutputStream(fos);
+            listaCirculos.add(new Circulo(9,6,20));
+            listaCirculos.add(new Circulo(-20,32,95));
+            salida.writeObject(listaCirculos);
+            fos.close();
+            salida.close();
+        }catch (FileNotFoundException fnfe){
+            System.out.println("Fichero no encontrado.");
+        }catch (EOFException eofe){
+            System.out.println("Fin de fichero.");
+        }catch (IOException ioe){
+            System.out.println("Error E/S");
+        }
+    }
+
+    public static void leerCirculo(){
+        try{
+            fis = new FileInputStream(RUTA_CIRCULOS);
+            entrada = new ObjectInputStream(fis);
+            listaCirculos = (ArrayList<Circulo>)entrada.readObject();
+            fis.close();
+            entrada.close();
+        }catch(EOFException eofe){
+            System.out.println("Fin de fichero.");
+        }catch (ClassNotFoundException cnfe){
+            System.out.println("Clase no encontrada.");
+        }catch (FileNotFoundException fnfe){
+            System.out.println("Fichero no encontrado.");
+        }catch (IOException ioe){
+            System.out.println("Error E/S.");
+        }
+    }
+
+    public static void mostrarCirculos(){
+        try{
+            fis = new FileInputStream(RUTA_CIRCULOS);
+            entrada = new ObjectInputStream(fis);
+            System.out.println((ArrayList<Circulo>)entrada.readObject());
+            fis.close();
+            entrada.close();
+        }catch(EOFException eofe){
+            System.out.println("Fin de fichero.");
+        }catch (ClassNotFoundException cnfe){
+            System.out.println("Clase no encontrada.");
+        }catch (FileNotFoundException fnfe){
+            System.out.println("Fichero no encontrado.");
+        }catch (IOException ioe){
+            System.out.println("Error E/S.");
+        }
+    }
+
+    public static void crearInformeCirculos(){
+        int contador = 1;
+        try{
+            fw = new FileWriter(RUTA_INFORME_CIRCULOS);
+            for(Circulo c:listaCirculos){
+                fw.write("Circulo nº"+contador+":\n");
+                fw.write(c.toString()+"\n\n");
+                contador++;
+            }
+            fw.close();
+        }catch (IOException ioe){
+            System.out.println("Error E/S.");
+        }
+    }
+
+    public static void mostrarFicheroTexto(){
+        FileReader fr = null;
+        try{
+            fr = new FileReader(RUTA_INFORME_CIRCULOS);
+            //mostrar todo en un bucle, letra a letra, luego linea a linea
+            System.out.println((char)fr.read());
+        }catch (EOFException eofe){
+            System.out.println("Error fin de fichero.");
+        }catch (FileNotFoundException fnfe){
+            System.out.println("Error fichero no encontrado.");
+        }catch (IOException ioe){
+            System.out.println("Error de E/S.");
         }
     }
 }
