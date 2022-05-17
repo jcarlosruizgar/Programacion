1f2ee2bcfa1631e8dea8f2c1354c3d32f9da0c44
jcarlosruizgar <jcarlosruizgar@gmail.com>
2022-05-17 22:48:10 +0200

    update

--- FigurasGeometricas/src/codigo/LecturaCirculo.java
+++ FigurasGeometricas/src/codigo/LecturaCirculo.java
@@ -10,37 +10,37 @@ public class LecturaCirculo {
         String cadena = null;
         int delA;
         int delB;
-        int longitud;
+        int longitud = 0;
         int x;
         int y;
         int radio;
-        final char DELIMITADOR ='#';
+        final char DELIMITADOR = '#';
         boolean salir = false;
-        try{
+        try {
             fr = new FileReader("./circulos.txt");
             br = new BufferedReader(fr);
 
-            while(!salir){
+            while (!salir) {
                 cadena = br.readLine();//cadena con los datos
                 String cadenaAuxiliar = cadena;//copia de la cadena
-                if (cadena==null) cadena =""; else
-                longitud = cadenaAuxiliar.length();//hasta donde llega el 3º dato
+                if (cadena == null) cadenaAuxiliar = "";//evita excepcion
+                else longitud = cadenaAuxiliar.length();//hasta donde llega el 3º dato
                 delB = cadenaAuxiliar.lastIndexOf(DELIMITADOR);//hasta donde llega le 2º dato
-                cadenaAuxiliar = cadenaAuxiliar.substring(0,delB);//se quita el ultimo delimitador
+                cadenaAuxiliar = cadenaAuxiliar.substring(0, delB);//se quita el ultimo delimitador
                 delA = cadenaAuxiliar.lastIndexOf(DELIMITADOR);//hasta donde llega el 1º dato
-                x = Integer.parseInt(cadena.substring(0,delA));//se guarda el 1º dato
-                y = Integer.parseInt(cadena.substring(delA+1,delB));//se guarda el 2º dato
-                radio = Integer.parseInt(cadena.substring(delB+1,longitud));//se guarda el 3º dato
-                System.out.println(new Circulo(x,y,radio));
+                x = Integer.parseInt(cadena.substring(0, delA));//se guarda el 1º dato
+                y = Integer.parseInt(cadena.substring(delA + 1, delB));//se guarda el 2º dato
+                radio = Integer.parseInt(cadena.substring(delB + 1, longitud));//se guarda el 3º dato
+                System.out.println(new Circulo(x, y, radio));
                 System.out.println(cadenaAuxiliar);
             }
-        }catch (StringIndexOutOfBoundsException sioobe){
+        } catch (StringIndexOutOfBoundsException sioobe) {
             salir = true;
-        }catch (EOFException eofException){
+        } catch (EOFException eofException) {
             System.out.println("Error, fin de fichero.");
-        }catch (FileNotFoundException fnfe){
+        } catch (FileNotFoundException fnfe) {
             System.out.println("Error, fichero no encontrado.");
-        }catch (IOException ioe){
+        } catch (IOException ioe) {
             System.out.println("Error, fallo de E/S.");
         }
 
