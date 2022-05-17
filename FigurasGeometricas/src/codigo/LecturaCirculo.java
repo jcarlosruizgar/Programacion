070e46f92bca2d2dbad14a592f1f1083e694e329
jcarlosruizgar <jcarlosruizgar@gmail.com>
2022-05-17 22:55:35 +0200

    update

--- FigurasGeometricas/src/codigo/LecturaCirculo.java
+++ FigurasGeometricas/src/codigo/LecturaCirculo.java
@@ -8,23 +8,21 @@ public class LecturaCirculo {
         FileReader fr = null;
         BufferedReader br = null;
         String cadena = null;
-        int delA;
-        int delB;
-        int longitud = 0;
+        int delA;//1º delimitador
+        int delB;//2º delimitador
+        int longitud = 0;//3º delimitador
         int x;
         int y;
         int radio;
         final char DELIMITADOR = '#';
-        boolean salir = false;
+        boolean salir = false;//variable bandera
         try {
             fr = new FileReader("./circulos.txt");
             br = new BufferedReader(fr);
-
             while (!salir) {
                 cadena = br.readLine();//cadena con los datos
                 String cadenaAuxiliar = cadena;//copia de la cadena
-                if (cadena == null) cadenaAuxiliar = "";//evita excepcion
-                else longitud = cadenaAuxiliar.length();//hasta donde llega el 3º dato
+                longitud = cadenaAuxiliar.length();//hasta donde llega el 3º dato
                 delB = cadenaAuxiliar.lastIndexOf(DELIMITADOR);//hasta donde llega le 2º dato
                 cadenaAuxiliar = cadenaAuxiliar.substring(0, delB);//se quita el ultimo delimitador
                 delA = cadenaAuxiliar.lastIndexOf(DELIMITADOR);//hasta donde llega el 1º dato
@@ -33,6 +31,9 @@ public class LecturaCirculo {
                 radio = Integer.parseInt(cadena.substring(delB + 1, longitud));//se guarda el 3º dato
                 System.out.println(new Circulo(x, y, radio));
             }
+            fr.close();
+            br.close();
+        } catch (NullPointerException ignored) {
         } catch (StringIndexOutOfBoundsException sioobe) {
             salir = true;
         } catch (EOFException eofException) {
@@ -41,6 +42,12 @@ public class LecturaCirculo {
             System.out.println("Error, fichero no encontrado.");
         } catch (IOException ioe) {
             System.out.println("Error, fallo de E/S.");
+        } finally {
+            try {
+                if (fr != null) fr.close();
+                if (br != null) br.close();
+            } catch (IOException ignored) {
+            }
         }
 
     }
