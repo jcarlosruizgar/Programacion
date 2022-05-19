77d882da9944c3df80f918d7743b7723270e935e
jcarlosruizgar <jcarlosruizgar@gmail.com>
2022-05-19 21:57:06 +0200

    update

--- GestionProvinciaMysql/src/codigo/Principal.java
+++ GestionProvinciaMysql/src/codigo/Principal.java
@@ -11,7 +11,6 @@ import java.sql.SQLIntegrityConstraintViolationException;
 import java.sql.Statement;
 
 
-
 public class Principal {
 
     private static Connection conexion;
@@ -36,7 +35,7 @@ public class Principal {
                 System.out.println("5 - Borrar una provincia.");
                 System.out.println("0 - Salir del programa.");
                 opcion = Integer.parseInt(br.readLine());
-                switch(opcion) {
+                switch (opcion) {
                     case 1:
                         mostrarProvincias();
                         break;
@@ -60,19 +59,21 @@ public class Principal {
                         System.out.println("Eso no es una opcion correcta.");
                         break;
                 }
-            }catch(IOException ioe) {
+            } catch (NumberFormatException nfe) {
+                System.out.println("Error, eso no es un numero.");
+            } catch (IOException ioe) {
                 System.out.println("Error de E/S.");
-            }catch(Exception e) {
+            } catch (Exception e) {
                 e.printStackTrace();
-            }finally {
+            } finally {
                 try {
-                    if(conexion != null) conexion.close();
-                    if(consulta != null) consulta.close();
-                    if(resultado != null) resultado.close();
-                }catch(Exception ignored) {
+                    if (conexion != null) conexion.close();
+                    if (consulta != null) consulta.close();
+                    if (resultado != null) resultado.close();
+                } catch (Exception ignored) {
                 }
             }
-        }while(!salir);
+        } while (!salir);
     }
 
     public static void mostrarProvincias() {
@@ -81,13 +82,13 @@ public class Principal {
             consulta = conexion.createStatement();
             resultado = consulta.executeQuery("select * from provincias_programacion;");
             while (resultado.next()) {
-                System.out.println("ID de provincia "+resultado.getInt("id")+
-                        " nombre de la provincia "+resultado.getString("nombre"));
+                System.out.println("ID de provincia " + resultado.getInt("id") +
+                        " nombre de la provincia " + resultado.getString("nombre"));
             }
             resultado.close();
             consulta.close();
             conexion.close();
-        }catch(SQLException ex) {
+        } catch (SQLException ex) {
             System.out.println("Error de conexion con la base de datos.");
         }
     }
@@ -96,65 +97,65 @@ public class Principal {
         try {
             conexion = DriverManager.getConnection(RUTADB, usuario, contrasena);
             consulta = conexion.createStatement();
-            consulta.executeUpdate("insert into provincias_programacion (nombre) values (\""+provincia+"\");");
+            consulta.executeUpdate("insert into provincias_programacion (nombre) values (\"" + provincia + "\");");
             consulta.close();
             conexion.close();
-        }catch(SQLException ex) {
+        } catch (SQLException ex) {
             ex.printStackTrace();
             System.out.println("Error de conexion con la base de datos.");
         }
     }
 
-    public static void insertarProvinciaInteractivo() throws IOException{
+    public static void insertarProvinciaInteractivo() throws IOException {
         System.out.println("Introduza el nombre de la provincia a insertar");
         String provincia = br.readLine();
         insertarProvincia(provincia);
     }
 
-    public static void modificarNombreProvincia(int id,String provincia) {
+    public static void modificarNombreProvincia(int id, String provincia) {
         try {
             conexion = DriverManager.getConnection(RUTADB, usuario, contrasena);
             consulta = conexion.createStatement();
-            consulta.executeUpdate("update provincias_programacion set nombre=\""+provincia+"\" where id="+id+";");
+            consulta.executeUpdate("update provincias_programacion set nombre=\"" + provincia + "\" where id=" + id + ";");
             consulta.close();
             conexion.close();
-        }catch(SQLException ex) {
+        } catch (SQLException ex) {
             ex.printStackTrace();
             System.out.println("Error de conexion con la base de datos.");
         }
     }
 
-    public static void modificarNombreProvinciaInteractivo() throws IOException{
+    public static void modificarNombreProvinciaInteractivo() throws IOException {
         mostrarProvincias();
         System.out.println("Introduza el id de la provincia a modificar:");
         int id = Integer.parseInt(br.readLine());
         System.out.println("Introduza el nuevo nombre de la provincia:");
         String nombre = br.readLine();
-        modificarNombreProvincia(id,nombre);
+        modificarNombreProvincia(id, nombre);
         System.out.println("Operacion realizada con exito.");
     }
 
-    public static void modificarIdProvincia(int viejoId,int nuevoId) {
+    public static void modificarIdProvincia(int viejoId, int nuevoId) {
         try {
             conexion = DriverManager.getConnection(RUTADB, usuario, contrasena);
             consulta = conexion.createStatement();
-            consulta.executeUpdate("update provincias_programacion set id="+nuevoId+" where id="+viejoId+";");
+            consulta.executeUpdate("update provincias_programacion set id=" + nuevoId + " where id=" + viejoId + ";");
             consulta.close();
             conexion.close();
-        }catch(SQLIntegrityConstraintViolationException sqlicve) {
+        } catch (SQLIntegrityConstraintViolationException sqlicve) {
             System.out.println("Error, ya existe ese id de provincia.");
-        }catch(SQLException ex) {
+        } catch (SQLException ex) {
             System.out.println("Error de conexion con la base de datos.");
         }
     }
 
-    public static void modificarIdProvinciaInteractivo() throws IOException{
+    public static void modificarIdProvinciaInteractivo() throws IOException {
         mostrarProvincias();
         System.out.println("Introduza el id de la provincia a modificar:");
         int viejoId = Integer.parseInt(br.readLine());
         System.out.println("Introduza el nuevo ie de la provincia:");
         int nuevoId = Integer.parseInt(br.readLine());
-        modificarIdProvincia(viejoId,nuevoId);
+        modificarIdProvincia(viejoId, nuevoId);
         System.out.println("Operacion realizada con exito.");
     }
 
@@ -162,15 +163,15 @@ public class Principal {
         try {
             conexion = DriverManager.getConnection(RUTADB, usuario, contrasena);
             consulta = conexion.createStatement();
-            consulta.executeUpdate("delete from provincias_programacion where id ="+id+";");
+            consulta.executeUpdate("delete from provincias_programacion where id =" + id + ";");
             consulta.close();
             conexion.close();
-        }catch(SQLException ex) {
+        } catch (SQLException ex) {
             System.out.println("Error de conexion con la base de datos.");
         }
     }
 
-    public static void borrarProvinciaInteractivo()throws IOException{
+    public static void borrarProvinciaInteractivo() throws IOException {
         mostrarProvincias();
         System.out.println("Introduza el id de la provincia a borrar:");
         int id = Integer.parseInt(br.readLine());
